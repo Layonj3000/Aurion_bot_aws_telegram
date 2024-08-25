fd3c428ca929045e68779e0fa67ea3a43855ad90
Pâmela Aliny Cleto Pavan <97994995+PamelaPavan@users.noreply.github.com>
2024-08-25 15:25:52 -0300

    :sparkles: feat: adicionando função get_telegram

--- src/transcribe.py
+++ src/transcribe.py
@@ -8,3 +8,13 @@ import boto3
 
 transcribe = boto3.client('transcribe')
 s3 = boto3.client('s3')
+
+def get_telegram_file_path(file_id):
+    print("def get_telegram_file_path(file_id):")
+    # Obter o caminho do arquivo no Telegram
+    tokenTelegram = os.getenv('tokenTelegram')
+    url = f"https://api.telegram.org/bot{tokenTelegram}/getFile?file_id={file_id}"
+    with urllib.request.urlopen(url) as response:
+        data = json.load(response)
+        return data['result']['file_path']
+
