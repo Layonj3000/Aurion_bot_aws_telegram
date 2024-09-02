92bf619e551bd5b047bad86bbb0ae9577e2e7ddc
Luiz Fillipe <luiz.fillipe.2000@hotmail.com>
2024-09-02 14:18:24 -0300

    :sparkles: feat: criação do update_user_state no dynamo.py

--- src/lambda/dynamo.py
+++ src/lambda/dynamo.py
@@ -5,3 +5,12 @@ from botocore.exceptions import ClientError
 
 dynamodb = boto3.client('dynamodb')
 table_name = os.getenv('tableName')
+
+def update_user_state(user_id, state):
+    dynamodb.put_item(
+        TableName=table_name,
+        Item={
+            'user_id': {'S': str(user_id)},
+            'state': {'S': state}
+        }
+    )
