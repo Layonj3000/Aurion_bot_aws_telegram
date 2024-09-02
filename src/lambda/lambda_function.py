4d4a632393d90dc0dc2a3cbf3ffe8998931d7c14
Luiz Fillipe <luiz.fillipe.2000@hotmail.com>
2024-09-02 14:06:01 -0300

    :sparkles: feat: adicionando lógica com dynamoDB para verificar o recurso que o usuário deseja

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -5,7 +5,8 @@ from dynamo import delete_user, get_user_state, update_user_state
 from textract import extract_text_from_image
 
 from lex_interaction import call_lex, process_lex_response
-from telegram_interaction import handle_non_text_message, send_message
+from telegram_interaction import (handle_non_text_message, save_image,
+                                  send_message)
 from transcribe import audio_user
 
 logger = logging.getLogger()
@@ -58,7 +59,43 @@ def lambda_handler(event, context):
 
             elif 'photo' in body['message']:
                 chat_id = body['message']['chat']['id']
-                handle_non_text_message(chat_id, body)
+
+                # Verificar se o usuário existe no banco de dados
+                user_state = get_user_state(chat_id)
+                save_image(chat_id, body)
+                
+                if user_state is None:
+                    # Se o usuário não existir, perguntar o que ele deseja fazer
+                    logger.info(f"Usuário {chat_id} não encontrado no banco de dados.")
+                    send_message(chat_id, "Olá! Como posso te ajudar hoje? Por favor, me diga o que deseja fazer.")
+                    buttons = [
+                        [{'text': 'Analisar Imagem', 'callback_data': 'Analisar Imagem'}],
+                        [{'text': 'Ler Rótulo de Imagem', 'callback_data': 'Rotulo'}]
+                    ]
+                    send_message(chat_id, "Escolha uma opção:", buttons)
+                    
+                else:
+                    # Se o usuário existir, continue com o fluxo normal
+                    logger.info(f"Usuário {chat_id} encontrado no banco de dados com estado: {user_state}")
+                    
+                    if user_state == 'ROTULO':
+                        
+                        rotulo = extract_text_from_image(chat_id)
+                        logger.info(f"Rotulo:  {rotulo}")
+                        delete_user(chat_id)
+                        send_message(chat_id, rotulo)
+                    
+                    elif user_state == 'ANALISAR':
+                        
+                        handle_non_text_message(chat_id)
+                        delete_user(chat_id)
+                        
+                    else:
+                        buttons = [
+                            [{'text': 'Analisar Imagem', 'callback_data': 'Analisar Imagem'}],
+                            [{'text': 'Ler Rótulo de Imagem', 'callback_data': 'Rotulo'}]
+                        ]
+                        send_message(chat_id, "Escolha uma opção:", buttons)
 
             elif 'text' in body['message']:
                 chat_id = body['message']['chat']['id']
